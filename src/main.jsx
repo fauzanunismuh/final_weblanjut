6045194ccb4232a93bbc73f13ec7dc8c989b8e13
Fauzan Azhari Rahman <fauzanazharirahman@consolelogmPro.local>
2024-02-29 09:33:15 +0800

    final fauzan azhari

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+// ==============================
+// = NAMA: FAUZAN AZHARI RAHMAN =
+// = NIM: 105841109622          =
+// = KELAS: 5C                  =
+// ==============================
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
